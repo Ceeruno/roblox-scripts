67ac0c19b7831287352a68422a5ef5208d27169c
Ceeruno <142674019+Ceeruno@users.noreply.github.com>
2024-05-17 19:03:01 -0300

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,1 +1,14 @@
+--[[Services]]
+local Players = Game:GetService("Players")
 
+--[[Instances]]
+local Player = Players.Misinformater
+local Mouse = Player:GetMouse()
+local Tool = Player.Backpack.Tool
+local Handle = Tool.Handle
+local RemoteEvent = Tool.RemoteEvent
+
+--[[Events]]
+Tool.Activated:Connect(function()
+		RemoteEvent:FireServer(Handle.Position, Mouse.Hit.p)
+end)
